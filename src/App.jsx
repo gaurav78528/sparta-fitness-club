4f85d44c719d004735939091002305420d09931e
mdyajul <yajul2530@gmail.com>
2022-12-13 17:33:02 +0530

    db file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Heading } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   const { appData } = useSelector((store) => store.app);
   console.log(appData);
   return (
     <div>
-      <Heading>Hello</Heading>
+      {/* <Heading>Hello</Heading> */}
+      <AllRoutes/>
     </div>
   );
 }
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import AdminHome from "../components/Admin/AdminHome";
 import Home from "../pages/Home";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/admin" element={<AdminHome />}>Admin</Route>
+      
     </Routes>
   );
 };
