a23bf2783ecea9311d8c018bbb50315bc37d7649
MasaiSchoo <30krishnakumar111998@gmail.com>
2022-12-15 01:46:32 +0530

    add

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import About from "../pages/about/About";
 import Home from "../pages/Home";
 import WorkOut from "../pages/Program/WorkOut";
 
@@ -11,6 +12,7 @@ const AllRoutes = () => {
       <Route path="/workout" element={<WorkOut />}></Route>
  
       {/* <Route path="/workout" element={<WorkOut />}></Route> */}
+      <Route path="/about" element={<About/>}></Route>
  
     
     </Routes>
