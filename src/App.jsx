784c04b2bef4ab7470aae5f65167c260347b4b67
gaurav78528 <gauravsuthar309@gmail.com>
2022-12-15 00:57:58 +0530

    fix conflits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { Heading } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import Navbar from "./components/Navbar/Navbar";
 import AllRoutes from "./Routes/AllRoutes";
-import Content from "./components/content/Content";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 function App() {
   const { appData } = useSelector((store) => store.app);
   console.log(appData);
@@ -11,9 +9,7 @@ function App() {
     <div>
       <Navbar />
       <AllRoutes />
-      <Content />
       <Footer />
-      <Heading>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut unde corrupti accusantium optio dolorum sit ipsam doloremque veritatis sint. Id maiores illum a nihil accusamus beatae inventore expedita, officiis illo.</Heading>
     </div>
   );
 }
