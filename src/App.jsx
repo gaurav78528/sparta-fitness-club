da764f8988242749fbbc87d24484855c2590984a
mdyajul <yajul2530@gmail.com>
2022-12-14 16:39:25 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Heading } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
-<<<<<<< HEAD
-import AllRoutes from "./Routes/AllRoutes";
-=======
  
-
 import AllRoutes from "./Routes/AllRoutes";
  
->>>>>>> 756c32c46de4ba5fdf4ebb0ed328e7799cdd0463
+ 
+ 
+ 
+ 
 
 function App() {
   const { appData } = useSelector((store) => store.app);
@@ -15,13 +14,13 @@ function App() {
   return (
     <div>
       <Heading>Hello</Heading>
-<<<<<<< HEAD
-      <AllRoutes/>
-=======
- 
- 
  
       <AllRoutes/>
+ 
+ 
+ 
+ 
+   
  
     </div>
   );
