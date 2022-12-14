91e1902df7f1cd8f803fbe8981b892ed08e34fd3
gaurav78528 <gauravsuthar309@gmail.com>
2022-12-15 01:02:56 +0530

    problems resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import AllRoutes from "./Routes/AllRoutes";
-
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 function App() {
   const { appData } = useSelector((store) => store.app);
   console.log(appData);
